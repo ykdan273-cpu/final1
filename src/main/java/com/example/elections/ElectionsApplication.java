dab5c97d796223ca47bf714686622a3967840c1a
Dan <ykdan273@gmail.com>
2026-01-12 16:22:40 +0300

    lecture1: init spring boot project

---
+++ src/main/java/com/example/elections/ElectionsApplication.java
@@ -0,0 +1,12 @@
+package com.example.elections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectionsApplication.class, args);
+    }
+}
